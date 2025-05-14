dd418b3ef270439726dc9276be9d51a1573850d0
pachyyy <adri.emma05@gmail.com>
2025-05-14 13:51:38 +0700

    dah

--- uas.py
+++ uas.py
@@ -143,9 +143,7 @@ class imageFrame(customtkinter.CTkFrame):
             # filtered = self.coherence_diffusion_filter(segmented, filter_params["sigma"])
             
             # Step 4: Apply log gabor filter
-            # gabor_filtered = self.log_gabor_filter(filtered, 
-            #                                      wavelength=filter_params["lambda"], 
-            #                                      orientation=filter_params["theta"])
+            # gabor_filtered = self.log_gabor_filter(filtered, wavelength=filter_params["lambda"], orientation=filter_params["theta"])
             
             # Step 5: Apply binarization
             final_image = self.binarization(segmented)
